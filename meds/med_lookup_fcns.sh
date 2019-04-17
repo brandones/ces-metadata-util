7c791bb4658b5fc9d79f8adbbf98da5af35289dd
Brandon Istenes <bistenes@gmail.com>
2019-04-17 16:35:07 -0500

    Add a bunch of meds data

--- meds/med_lookup_fcns.sh
+++ meds/med_lookup_fcns.sh
@@ -1,3 +1,3 @@
 function medlookup { grep -i "$1" results/meds-matches-* ; }
 function medgrep { grep -i $1 input/HUM_Drug_List-13.csv input/meds-ciel.json ; }
-function medgrep2 { grep -i $1 HUM_Drug_List-13.csv meds-ciel.json | grep -i $2 ; }
+function medgrep2 { grep -i $1 input/HUM_Drug_List-13.csv input/meds-ciel.json | grep -i $2 ; }
